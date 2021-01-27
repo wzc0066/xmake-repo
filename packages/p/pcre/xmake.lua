ebfabbe9e63bd53ab9db60753a0910138f731364
wzc0066 <zwang@uc3dp.com>
2021-01-27 18:55:21 +0800

    add pcre build

--- packages/p/pcre/xmake.lua
+++ packages/p/pcre/xmake.lua
@@ -3,61 +3,26 @@ package("pcre")
     set_homepage("https://www.pcre.org/")
     set_description("A Perl Compatible Regular Expressions Library")
 
-    set_urls("https://ftp.pcre.org/pub/pcre/pcre-$(version).zip",
-             "ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-$(version).zip")
+    set_urls("http://117.143.63.254:9012/www/rt-smart/packages/pcre-$(version).tar.gz")
 
-    add_versions("8.40", "99e19194fa57d37c38e897d07ecb3366b18e8c395b36c6d555706a7f1df0a5d4")
-    add_versions("8.41", "0e914a3a5eb3387cad6ffac591c44b24bc384c4e828643643ebac991b57dfcc5")
+    add_versions("8.44", "aecafd4af3bd0f3935721af77b889d9024b2e01d96b58471bd91a3063fb47728")
 
-    if is_host("windows") and not is_plat("mingw") then
-        add_deps("cmake")
-    end
-
-    add_configs("jit", {description = "Enable jit.", default = true, type = "boolean"})
-    add_configs("bitwidth", {description = "Set the code unit width.", default = "8", values = {"8", "16", "32"}})
-
-    on_load(function (package)
-        local bitwidth = package:config("bitwidth") or "8"
-        package:add("links", "pcre" .. (bitwidth ~= "8" and bitwidth or ""))
-        if not package:config("shared") then
-            package:add("defines", "PCRE_STATIC")
-        end
-    end)
-
-    on_install("windows", function (package)
-        local configs = {"-DPCRE_BUILD_TESTS=OFF"}
-        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
-        table.insert(configs, "-DPCRE_SUPPORT_JIT=" .. (package:config("jit") and "ON" or "OFF"))
-        local bitwidth = package:config("bitwidth") or "8"
-        if bitwidth ~= "8" then
-            table.insert(configs, "-DPCRE_BUILD_PCRE8=OFF")
-            table.insert(configs, "-DPCRE_BUILD_PCRE" .. bitwidth .. "=ON")
-        end
-        if package:debug() then
-            table.insert(configs, "-DPCRE_DEBUG=ON")
-        end
-        import("package.tools.cmake").install(package, configs)
-    end)
-
-    on_install("macosx", "linux", "mingw", function (package)
-        local configs = {}
-        if package:config("shared") then
-            table.insert(configs, "--enable-shared=yes")
-        else
-            table.insert(configs, "--enable-shared=no")
-        end
-        if package:config("jit") then
-            table.insert(configs, "--enable-jit")
-        end
-        local bitwidth = package:config("bitwidth") or "8"
-        if bitwidth ~= "8" then
-            table.insert(configs, "--disable-pcre8")
-            table.insert(configs, "--enable-pcre" .. bitwidth)
-        end
-        if package:debug() then
-            table.insert(configs, "--enable-debug")
-        end
-        import("package.tools.autoconf").install(package, configs)
+    on_install("cross", function (package)
+	--import("core.base.option")
+        local CROSS_COMPILE="arm-linux-musleabi"
+	local configs = {"--prefix=" .. package:installdir(), "--target=" .. CROSS_COMPILE, "--host=" .. CROSS_COMPILE, "--build=i686-pc-linux-gnu", "--disable-shared"}
+        local buildenvs = import("package.tools.autoconf").buildenvs(package)
+        os.vrunv("./configure", configs, {envs = buildenvs})
+	io.gsub("libtool", "\nlt_ar_flags=\n", "\nlt_ar_flags=cr\n")
+	io.gsub("libtool", "\n    prefer_static_libs=no\n", "\n    prefer_static_libs=yes\n")
+	--##local makeconfigs = {V=1, CFLAGS = buildenvs.CFLAGS, LDFLAGS = buildenvs.LDFLAGS}
+	--##import("package.tools.make").install(package, makeconfigs)
+	local argv = {"-j1"}
+        table.insert(argv, "V=1")
+        table.insert(argv, "CFLAGS=" .. buildenvs.CFLAGS)
+        table.insert(argv, "LDFLAGS=" .. buildenvs.LDFLAGS)
+	os.vrunv("make", argv)
+	os.vrun("make install")
     end)
 
     on_test(function (package)
